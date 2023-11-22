08c7926ac55e030470bb686873869fb13a647ee1
Yasha <yashahelfi2012@gmail.com>
2023-11-22 15:44:07 +0300

    Split core and backend into two git repos

--- backend/api/users/api_models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-from pydantic import BaseModel, EmailStr, Field
-
-
-class FeedbackForm(BaseModel):
-    email: Optional[EmailStr] = Field(None, description='Pass for non-authorized users')
-    name: Optional[str] = Field(None, description='Pass for non-authorized users')
-    text: str
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent
+README_DIR = BASE_DIR / 'readme.md'
+REQUIREMENTS_DIR = BASE_DIR / 'requirements.txt'
+
+long_description = README_DIR.read_text()
+requirements = [line for line in REQUIREMENTS_DIR.read_text().split('\n') if line and not line.startswith('#')]
+
+
+setup(
+    name='scraperai',
+    version='0.0.1',
+    description='Auto-parsing library for Python',
+    url='https://github.com/iakov-kaiumov/scraperai',
+    author='Iakov Kaiumov',
+    author_email='kaiumov.iag@phystech.edu',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT License',
+    packages=find_packages(),
+    package_dir={'scraperai': 'core'},
+    install_requires=requirements
+)
