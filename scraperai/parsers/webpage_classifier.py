53308d6f32386a74cc7df30e09c07a46f4657238
Yasha <yashahelfi2012@gmail.com>
2024-05-21 17:40:32 +0300

    Implement WebpageTextClassifier

--- scraperai/parsers/webpage_classifier.py
+++ scraperai/parsers/webpage_classifier.py
@@ -1,9 +1,11 @@
 from typing import Optional
 
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_text_splitters import TokenTextSplitter
 
 from scraperai.parsers.agent import ChatModelAgent
 from scraperai.models import WebpageType
+from scraperai.utils.html import minify_html
 from scraperai.utils.image import encode_image_to_b64
 from scraperai.lm.base import BaseVision, BaseJsonLM
 
@@ -49,19 +51,42 @@ Return only category name in the answer.
         return WebpageType(text)
 
 
-class WebpageTextClassifier:
+class WebpageTextClassifier(ChatModelAgent):
     def __init__(self, model: BaseJsonLM):
-        self.model = model
+        super().__init__(model)
+        self.max_chunk_size = 12000
 
     def classify(self, html_content: str) -> WebpageType:
-        # TODO: Add prompt
+        compressed_html, subs = minify_html(html_content, good_attrs={'class', 'href'})
+        html_part = TokenTextSplitter(chunk_size=self.max_chunk_size).split_text(compressed_html)[0]
+
+        system_prompt = f"""
+Your goal is to classify website by HTML into 4 categories - {WebpageType.values_repr()}.
+"catalog" page contains a table or a list of similar elements or cards.
+"detailed_page" contains description of one product.
+In other cases return "other".
+Return only one category in a form of json. Example of response json:
+```
+{{
+    "category": "catalog"
+}}
+```
+"""
         messages = [
             SystemMessage(
-                content="Your goal is to classify webpages by screenshot."
+                content=system_prompt
             ),
             HumanMessage(
-                content=""
-            ),
+                content=html_part
+            )
         ]
-        response = self.model.invoke(messages)
-        return WebpageType(response)
+
+        def _validate_response(response: dict) -> Optional[str]:
+            try:
+                WebpageType(response['category'])
+            except ValueError:
+                return f"{response} is not a valid page type. Should be one of {WebpageType.values_repr()}"
+            return None
+
+        data: dict = self.query_with_validation(messages, _validate_response, max_retries=2)
+        return WebpageType(data['category'])
