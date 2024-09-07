75056df035abcaab1b092e95f7f4f04934b2ae0f
Anna Lisachenko <lisachenko.am@phystech.edu>
2024-09-07 12:04:56 +0300

    Update to 0.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requirements = [line for line in REQUIREMENTS_DIR.read_text().split('\n') if lin
 
 setup(
     name='scraperai',
-    version='0.0.4',
+    version='0.0.5',
     description='ScraperAI is an open-source, AI-powered tool designed to simplify web scraping for users of all skill levels.',
     url='https://github.com/scraperai/scraperai',
     author='Iakov Kaiumov',
